f998a39c87a4c501ebf1e879828a6e03a8e33c42
wabilin <twelendil@protonmail.com>
2024-05-04 14:57:07 +0900

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import ImagePreview from './components/ImagePreview';
 
 const App: Component = () => {
   return (
-    <div class='flex flex-col items-center p-8'>
-      <h1 class='text-2xl font-semibold text-zinc-300 mb-5'>
+    <div class='flex flex-col items-center p-8 space-y-5'>
+      <h1 class='text-2xl font-semibold text-zinc-300'>
         Photo Layout on Twitter Preview
       </h1>
       <div class='flex flex-col items-stretch gap-5'>
         <ImagePreview />
         <ImageUploader />
       </div>
+      <footer class='p-8'>
+        <a
+          class='text-zinc-300 flex items-center gap-1 text-sm'
+          href='https://github.com/wabilin/twitter-photo-preview'
+        >
+          <img src='/src/assets/github-mark-white.svg' alt='GitHub' class='w-4 h-4' />
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 };
